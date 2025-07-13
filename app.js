076f0bd670426eaf4d55b7aaad334185c602397e
Simple Agarwal <agarwalsimple71@gmail.com>
2025-07-13 20:38:21 +0530

    Modified

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
 const path=require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride=require("method-override");
-const ejsMate= require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
+const session=require("express-session");
+const flash=require("connect-flash");
+
+
+
+
+
+const listings=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
 
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
@@ -30,6 +37,18 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const sessionOptions={
+    secret:"mysupersecretcode",
+    resave:false,
+    saveUninitialised:true,
+    cookie:{
+        expires:Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24* 60 * 60 *1000,
+        httpOnly:true,
+    },
+
+};
+
 
 //API CREATION
 app.get("/",(req,res)=>{
@@ -37,78 +56,18 @@ app.get("/",(req,res)=>{
 });
 
 
-//INDEX ROUTE
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings= await Listing.find({});
-        res.render("listings/index.ejs",{allListings});
-    }));
+app.use(session(sessionOptions));
+app.use(flash());
 
-    
-    //New ROute
-    app.get("/listings/new",wrapAsync(async (req,res)=>{
-        res.render("listings/new.ejs");
-    }));
-
-    //show route
-    app.get("/listings/:id",wrapAsync(async (req,res)=>{
-        let {id}=req.params;
-        const listing=await Listing.findById(id);
-        res.render("listings/show.ejs",{listing});
-    }));
-
-    //create route
-    app.post("/listings",wrapAsync(async (req,res,next) => {
-            const newListing= new Listing(req.body.listing);
-            await newListing.save();
-            res.redirect("/listings"); 
-        })
-        
-    );
-
-    //EDIT ROUTE
-    app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
-        let {id}=req.params;
-        const listing=await Listing.findById(id);
-        res.render("listings/edit.ejs",{listing});
-}));
-
-    //UPDATE ROUTE
-
-    app.put("/listings/:id",wrapAsync(async (req,res)=>{
-        let {id}=req.params;
-        await Listing.findByIdAndUpdate(id, {...req.body.listing});
-        res.redirect(`/listings/${id}`);
-    }));
-
-    //DELETE ROUTE
-
-    app.delete("/listings/:id",wrapAsync(async (req,res)=>{
-        let {id}=req.params;
-        let deletedListing = await Listing.findByIdAndDelete(id);
-        console.log(deletedListing);
-        res.redirect("/listings");
-    }));
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+    next();
+});
 
 
 
-
-
-
-
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing= new Listing({
-//         title:"My New Villa",
-//         description:"By the Beach",
-//         price:1200,
-//         location:"calangute,Goa",
-//         country:"India",
-//     });
-//     await sampleListing.save();
-//     console.log("Sample was Saved");
-//     res.send("successful testing");
-
-// });
+app.use("/listings",listings);
+app.use("/listings/:id/reviews", reviews);
 
 app.all("*", (req,res,next) => {
     next(new ExpressError(404,"Page Not found!"));
@@ -117,10 +76,12 @@ app.all("*", (req,res,next) => {
 
 app.use((err,req,res,next)=>{
     let{statusCode=500, message="something went wrong!"} = err;
-    res.status(statusCode).send(message);
-});
+    res.status(statusCode).render("error.ejs",{message});
+    //res.status(statusCode).send(message);
 
+});
 
 app.listen(8080, ()=>{
     console.log("server is listening to port 8080");
 });
+
